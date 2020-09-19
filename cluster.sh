f78944e6e61ae24076f10d245c6ea3100d13bc4a
Nenad Miladinovic <nenadmiladin@yahoo.com>
2020-09-19 20:55:51 +0200

    Update cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,4 +1,5 @@
 
+az group create --name myResourceGroup --location eastus
 
 az aks create --resource-group myResourceGroup --name myAKSCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys
 

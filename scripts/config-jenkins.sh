669483e68de7b09f7a27ad74e66129bbfcc14c19
Nenad Miladinovic <nenadmiladin@yahoo.com>
2020-09-20 16:11:24 +0200

    Update config-jenkins.sh

--- scripts/config-jenkins.sh
+++ scripts/config-jenkins.sh
@@ -18,7 +18,7 @@ sudo apt-get update
 sudo apt-get install docker-ce -y
 
 # Azure CLI
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -26,7 +26,7 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
-sudo apt-get install azure-cli
+sudo apt-get install azure-cli -y
 
 # Kubectl
 cd /tmp/

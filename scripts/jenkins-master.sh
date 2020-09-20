d16b68f1c5004d59476741f6aac2ccefa2763432
Nenad Miladinovic <nenadmiladin@yahoo.com>
2020-09-20 22:46:02 +0200

    Update jenkins-master.sh

--- scripts/jenkins-master.sh
+++ scripts/jenkins-master.sh
@@ -21,11 +21,7 @@ then
 
     # Open port 8080 to allow web traffic to host.
     az vm open-port --port 8080 --resource-group $resourceGroup --name $virtualMachine --priority 103
-
-    # Use CustomScript extension to install NGINX.
-#    az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $virtualMachine --resource-group $resourceGroup --settings '{"fileUris": ["https://raw.githubuser
-#content.com/nenadmiladin/project-x/master/scripts/config-jenkins.sh"],"commandToExecute": "./config-jenkins.sh"}'
-
+    
     # Get public IP
     ip=$(az vm list-ip-addresses --resource-group $resourceGroup --name $virtualMachine --query [0].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv)
 
